f0f3891ac1aeb427b7c370b9d73e83019543e1ab
ericminio <eric.mignot@gmail.com>
2024-05-24 13:44:22 -0700

    ready

--- about/1.vision.test.js
+++ about/1.vision.test.js
@@ -4,26 +4,26 @@ import { eventually, Page } from "../yop/dist/index.js";
 import { server } from "../app/web/server.js";
 
 describe("Vision", () => {
-    let page;
-    let baseUrl;
-    before(async () => {
-        const port = await server.start();
-        baseUrl = `http://localhost:${port}`;
-    });
-    after(async () => {
-        await server.stop();
-    });
-    beforeEach(async () => {
-        page = new Page();
-        await page.open(baseUrl);
-    });
-    afterEach(async () => {
-        await page.close();
-    });
+  let page;
+  let baseUrl;
+  before(async () => {
+    const port = await server.start();
+    baseUrl = `http://localhost:${port}`;
+  });
+  after(async () => {
+    await server.stop();
+  });
+  beforeEach(async () => {
+    page = new Page();
+    await page.open(baseUrl);
+  });
+  afterEach(async () => {
+    await page.close();
+  });
 
-    it("is great", async () => {
-        await eventually(page, async () => {
-            assert.match(await page.section("Hello World!"), /.*/);
-        });
+  it("is great", async () => {
+    await eventually(page, async () => {
+      assert.match(await page.section("Hello World!"), /.*/);
     });
+  });
 });
--- app/start.mjs
+++ app/start.mjs
@@ -1,7 +1,7 @@
-import { server } from './web/server.js';
+import { server } from "./web/server.js";
 
-if (!process.argv[1].endsWith('test.js')) {
-    server.start((port) => {
-        console.log(`listening on port ${port}`);
-    });
+if (!process.argv[1].endsWith("test.js")) {
+  server.start((port) => {
+    console.log(`listening on port ${port}`);
+  });
 }
