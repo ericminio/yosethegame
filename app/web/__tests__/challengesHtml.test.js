75128fb899de57b50edc704378fdfa5f16568354
ericminio <eric.mignot@gmail.com>
2025-04-24 23:43:32 -0700

    faster

--- app/web/__tests__/challengesHtml.test.js
+++ app/web/__tests__/challengesHtml.test.js
@@ -1,6 +1,6 @@
 import { describe, it } from "node:test";
 import { strict as assert } from "node:assert";
-import { expose } from "../../../yop/dist/index.js";
+import { expose } from "../../../yop/dist/code/expose.js";
 
 const challengeSectionHtml = expose({
   symbol: "challengeSectionHtml",
@@ -13,14 +13,14 @@ describe("Challenges section html", () => {
   it("is as expected for open challenge", () => {
     assert.equal(
       oneliner(challengeSectionHtml({ name: "Hello Yose", open: true })),
-      '<section><h2>Hello Yose</h2><label id="challenge-hello-yose-status">open</label></section>',
+      '<section><h2>Hello Yose</h2><label id="challenge-hello-yose-status">open</label></section>'
     );
   });
 
   it("is as expected for closed challenge", () => {
     assert.equal(
       oneliner(challengeSectionHtml({ name: "Hello Yose", open: false })),
-      '<section><h2>Hello Yose</h2><label id="challenge-hello-yose-status">closed</label></section>',
+      '<section><h2>Hello Yose</h2><label id="challenge-hello-yose-status">closed</label></section>'
     );
   });
 });
