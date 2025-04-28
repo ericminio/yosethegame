705bdf3f59c04138227f8d83c11afcd5ede4ece9
Eric Mignot <eric.mignot@gmail.com>
2025-04-28 13:22:39 -0700

    scor emagic number removed

--- app/web/running.js
+++ app/web/running.js
@@ -3,10 +3,14 @@ import { challengeStatusId } from "./rendering.js";
 export const run = async (document, challenges) => {
   const playerServerUrl = document.getElementById("url").value;
   const openChallenges = challenges.filter((challenge) => challenge.open);
+  let score = 0;
   for (const challenge of openChallenges) {
     const { name, play } = challenge;
     const result = await play(playerServerUrl);
     document.getElementById(challengeStatusId(name)).innerHTML = result;
+    if (/passed/.test(result)) {
+      score += 10;
+    }
   }
-  document.getElementById("score").innerHTML = "10";
+  document.getElementById("score").innerHTML = `${score}`;
 };
