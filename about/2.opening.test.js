eb7b024e4b84945bca902adb5a517192df18b2d5
Eric Mignot <eric.mignot@gmail.com>
2025-04-29 21:31:08 -0700

    server less

--- about/2.opening.test.js
+++ about/2.opening.test.js
@@ -1,28 +1,24 @@
 import { describe, it, before, after, beforeEach, afterEach } from "node:test";
 import { strict as assert } from "node:assert";
+import { URL } from "node:url";
 import { eventually } from "../yop/testing/eventually.js";
 import { Page } from "../yop/testing/page.js";
-import { server } from "../app/server.js";
 import { playerServer } from "./playing/player-server-passing.js";
 
 describe("Yose the game", () => {
   let page;
-  let baseUrl;
 
   let playerServerUrl;
   before(async () => {
-    const port = await server.start();
-    baseUrl = `http://localhost:${port}`;
     const playerServerPort = await playerServer.start();
     playerServerUrl = `http://localhost:${playerServerPort}`;
   });
   after(async () => {
     await playerServer.stop();
-    await server.stop();
   });
   beforeEach(async () => {
     page = new Page();
-    await page.open(baseUrl);
+    await page.open(new URL("../app/web/assets/index.html", import.meta.url));
   });
   afterEach(async () => {
     await page.close();
