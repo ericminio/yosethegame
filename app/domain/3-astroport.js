09dc556999a623a7c86db27afcd6112cfeb93075
Eric Mignot <eric.mignot@gmail.com>
2025-05-04 09:59:11 -0700

    astropport

--- app/domain/3-astroport.js
+++ app/domain/3-astroport.js
@@ -3,7 +3,10 @@ import { Challenge } from "./challenge.js";
 
 export class Astroport extends Challenge {
   constructor() {
-    super("Astroport", "Update your server for ... (coming soon)");
+    super(
+      "Astroport",
+      "Update your server for <code>/astroport</code> to return a web page containing <code>#astroport-name</code>.",
+    );
   }
 
   open(store) {
@@ -18,7 +21,25 @@ export class Astroport extends Challenge {
     return false;
   }
 
-  async play() {
-    return { status: "failed" };
+  async play(playerServerUrl) {
+    const response = await fetch(`${playerServerUrl}/astroport`);
+    const status = response.status;
+    const contentType = response.headers.get("content-type");
+    const content = await response.text();
+
+    const expected = {
+      status: 200,
+      contentType: "text/html",
+      content: "A web page containing #astroport-name",
+    };
+    return status === expected.status &&
+      contentType === expected.contentType &&
+      content.indexOf('id="astroport-name"') !== -1
+      ? { status: "passed" }
+      : {
+          status: "failed",
+          expected,
+          actual: { status, contentType, content },
+        };
   }
 }
--- app/domain/__tests__/3-astroport.test.js
+++ app/domain/__tests__/3-astroport.test.js
@@ -18,4 +18,58 @@ describe("Astroport challenge", () => {
     store.save(new HelloYose().name, { status: "passed" });
     assert.deepEqual(astroport.open(store), true);
   });
+
+  it("hits /astroport of player", async (t) => {
+    t.mock.method(global, "fetch", async (url) => ({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html",
+      }),
+      text: async () => `${url}`,
+    }));
+    const result = await astroport.play("server-url");
+
+    assert.equal(result.actual.content, "server-url/astroport");
+  });
+
+  it("requires a html page with expected content", async (t) => {
+    t.mock.method(global, "fetch", async () => ({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html",
+      }),
+      text: async () =>
+        '<html><body><label id="astroport-name">the best astroport ever</label></body></html>',
+    }));
+    const result = await astroport.play();
+
+    assert.deepEqual(result, { status: "passed" });
+    t.mock.restoreAll();
+  });
+
+  it("discloses expectations when received answer is wrong", async (t) => {
+    t.mock.method(global, "fetch", async () => ({
+      status: 200,
+      headers: new Headers({
+        "content-type": "text/html",
+      }),
+      text: async () => "<html><body>nope</body></html>",
+    }));
+    const result = await astroport.play();
+
+    assert.deepEqual(result, {
+      status: "failed",
+      expected: {
+        status: 200,
+        contentType: "text/html",
+        content: "A web page containing #astroport-name",
+      },
+      actual: {
+        status: 200,
+        contentType: "text/html",
+        content: "<html><body>nope</body></html>",
+      },
+    });
+    t.mock.restoreAll();
+  });
 });
