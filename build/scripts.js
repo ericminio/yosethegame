6537215808d159c37c5bf3d393b599aa16814d5a
Eric Mignot <eric.mignot@gmail.com>
2025-05-09 06:17:15 -0700

    build

--- build/scripts.js
+++ build/scripts.js
@@ -3,10 +3,10 @@ import { stringify } from "./stringifier.js";
 import {
   primeFactorsOf,
   powerOfTwoChooser,
-} from "../../domain/4-power-of-two-lib.js";
-import { stringGuardChooser } from "../../domain/5-string-guard-lib.js";
-import { Store } from "../../domain/store.js";
-import { wireEvents } from "../wiring.js";
+} from "../app/domain/4-power-of-two-lib.js";
+import { stringGuardChooser } from "../app/domain/5-string-guard-lib.js";
+import { Store } from "../app/domain/store.js";
+import { wireEvents } from "../app/web/wiring.js";
 import {
   dashName,
   challengeResultId,
@@ -16,20 +16,20 @@ import {
   challengeSectionInnerHtml,
   renderChallenge,
   renderRunTrigger,
-} from "../rendering.js";
-import { run } from "../../domain/running.js";
-import { StringGuard } from "../../domain/5-string-guard.js";
-import { HelloYose } from "../../domain/1-hello-yose.js";
-import { Ping } from "../../domain/2-ping.js";
-import { PowerOfTwo } from "../../domain/4-power-of-two.js";
-import { Astroport } from "../../domain/3-astroport.js";
-import { Challenge } from "../../domain/challenge.js";
-import { Gates } from "../../domain/6-gates.js";
-import { Dock } from "../../domain/7-dock.js";
-import { shipChooser } from "../../domain/7-dock-lib.js";
-import { Keep } from "../../domain/8-keep.js";
-import { ChallengeAstroport } from "../../domain/challenge-astroport.js";
-import { Full } from "../../domain/9-full.js";
+} from "../app/web/rendering.js";
+import { run } from "../app/domain/running.js";
+import { StringGuard } from "../app/domain/5-string-guard.js";
+import { HelloYose } from "../app/domain/1-hello-yose.js";
+import { Ping } from "../app/domain/2-ping.js";
+import { PowerOfTwo } from "../app/domain/4-power-of-two.js";
+import { Astroport } from "../app/domain/3-astroport.js";
+import { Challenge } from "../app/domain/challenge.js";
+import { Gates } from "../app/domain/6-gates.js";
+import { Dock } from "../app/domain/7-dock.js";
+import { shipChooser } from "../app/domain/7-dock-lib.js";
+import { Keep } from "../app/domain/8-keep.js";
+import { ChallengeAstroport } from "../app/domain/challenge-astroport.js";
+import { Full } from "../app/domain/9-full.js";
 const codes = [
   `${Challenge.toString()};`,
   `${ChallengeAstroport.toString()};`,
